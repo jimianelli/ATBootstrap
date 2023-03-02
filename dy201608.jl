cd19115a9676e5ca46b9e0c53945a8f22d8103c8
Sam Urmy <oceanographerschoice@gmail.com>
2023-03-02 14:39:23 -0900

    Create scripts 2012-2016 (not working)
    Waiting for database updates from Kresimir before these run.

---
+++ dy201608.jl
@@ -0,0 +1,93 @@
+using CSV, DataFrames, DataFramesMeta
+using GeoStats, GeoStatsPlots
+using Statistics, StatsBase
+using Distributions
+using Random
+using ConcaveHull
+using StatsPlots, StatsPlots.PlotMeasures
+
+using Revise
+includet(joinpath(@__DIR__, "src", "ATBootstrap.jl"))
+
+surveydir = joinpath(@__DIR__, "surveydata", "201608")
+resolution = 10.0 # km
+const km2nmi = 1 / 1.852
+
+acoustics, scaling, trawl_locations = read_survey_files(surveydir)
+
+scaling_classes = unique(scaling.class)
+
+acoustics = @chain acoustics begin
+    @subset(in(scaling_classes).(:class), :transect .< 200)
+    DataFramesMeta.@transform(:x = round.(:x, digits=-1), :y = round.(:y, digits=-1))
+    @by([:transect, :class, :x, :y], 
+        :lon = mean(:lon), :lat = mean(:lat), :nasc = mean(:nasc))
+end
+@df acoustics scatter(:x, :y, group=:class, markersize=:nasc/500, markerstrokewidth=0, alpha=0.5)
+@df trawl_locations scatter!(:x, :y, label="")
+
+surveydata = ATSurveyData(acoustics, scaling, trawl_locations, surveydomain)
+
+cal_error = 0.1 # dB
+dA = (resolution * km2nmi)^2
+class_problems = map(scaling_classes) do class
+    println(class)
+    return ATBootstrapProblem(surveydata, class, cal_error, dA, nreplicates=1000)
+end
+
+simdomain = solution_domain(class_problems[1])
+sim_fields = [nonneg_lusim(p) for p in class_problems]
+sim_plots = map(enumerate(sim_fields)) do (i, x)
+    plot(simdomain, zcolor=x, clims=(0, quantile(x, 0.999)), 
+        markerstrokewidth=0, markershape=:square, title=string(scaling_classes[i]),
+        markersize=2.5, xlabel="Easting (km)", ylabel="Northing (km)")
+    df = @subset(acoustics, :class .== scaling_classes[i])
+    scatter!(df.x, df.y, color=:white, markersize=df.nasc*1e-3, alpha=0.3,
+        markerstrokewidth=0)
+end
+plot(sim_plots..., size=(1000, 1000))
+# i = 1
+# x = nonneg_lusim(params[i], zdists[i])
+# plot(domain(sol), zcolor=x, markerstrokewidth=0, markershape=:square, clims=(0, quantile(x, 0.999)), 
+#     title=string(scaling_classes[i]), markersize=4.2, background_color=:black, 
+#     xlabel="Easting (km)", ylabel="Northing (km)", size=(1000, 1000))
+
+
+# BROKEN: scaling data only has one event_id for some reason.
+# Need to check w/ Kresimir.
+unique(trawl_locations.event_id)
+unique(scaling.event_id)
+
+results = simulate_classes(class_problems, surveydata)
+
+
+@df results density(:n_age/1e9, group=:age, #xlims=(0, 8),
+    fill=true, alpha=0.7, ylims=(0, 25), palette=:Paired_10,
+    xlabel="Billions of fish", ylabel="Probability density")
+
+@df results boxplot(:age, :n_age/1e9, group=:age, palette=:Paired_10,
+    ylabel="Billions of fish")
+
+@chain results begin
+    @orderby(:age)
+    @by(:age, 
+        :n_age = mean(:n_age),
+        :std_age = std(:n_age), 
+        :cv_age = std(:n_age) / mean(:n_age) * 100)
+end
+
+
+@df results density(:biomass_age/1e9, group=:age, #xlims=(0, 4),
+    fill=true, alpha=0.7, ylims=(0, 50), palette=:Paired_10,
+    xlabel="Million tons", ylabel="Probability density")
+
+@df results boxplot(:age, :biomass_age/1e9, group=:age, palette=:Paired_10,
+    ylabel="Million tons")
+
+@chain results begin
+    @orderby(:age)
+    @by(:age, 
+        :biomass_age = mean(:biomass_age),
+        :std_age = std(:biomass_age), 
+        :cv_age = std(:biomass_age) / mean(:biomass_age) * 100)
+end
