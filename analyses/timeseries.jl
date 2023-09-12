8394e9f01daf3474f80765ff3d073c9c59cceb9d
Sam Urmy <oceanographerschoice@gmail.com>
2023-09-11 23:02:31 -0700

    update timeseries script for all analyses

--- analyses/timeseries.jl
+++ analyses/timeseries.jl
@@ -1,5 +1,5 @@
 using CSV, DataFrames, DataFramesMeta, CategoricalArrays
-using Statistics
+using Statistics, StatsBase
 using StatsPlots, StatsPlots.PlotMeasures
 using ColorSchemes
 
@@ -36,12 +36,12 @@ p_n = @df @subset(annual, :variable .== "n_age") plot(:year, :value,
     ribbon = (:value .- :lower, :upper .- :value), 
     series_annotation=text.(" " .* string.(:cv), :left, :bottom, 10),
     marker=:o, color=1, label="",
-    xticks=2012:2022, ylims=(0, 32),
+    xticks=2007:2022, ylims=(0, 32),
     xlabel="Year", ylabel="Abundance (billions)")
 p_b = @df @subset(annual, :variable .== "biomass_age") plot(:year, :value, 
     ribbon = (:value .- :lower, :upper .- :value), marker=:o, color=2, label="",
     series_annotation=text.(" " .* string.(:cv), :left, :bottom, 10),
-    xticks=2012:2022, ylims=(0, 7.5),
+    xticks=2007:2022, ylims=(0, 7.5),
     xlabel="Year", ylabel="Biomass (MT)")
 plot(p_n, p_b, layout=(2, 1), size=(800, 600), margin=20px)
 savefig(joinpath(@__DIR__, "timeseries.png"))
@@ -53,7 +53,7 @@ plots_n = map(unique(results.year)) do year
         xticks=1:10, xlabel="", ylabel="")
     # scatter!(p, [1], [0], alpha=0, label=year)
 end
-plot(plots_n..., layout=(5, 2), size=(600, 600), left_margin=20px)
+plot(plots_n..., layout=(10, 1), size=(600, 900), left_margin=20px, xticks=false)
 savefig(joinpath(@__DIR__, "abundance.png"))
 
 plots_b = map(unique(results.year)) do year
@@ -62,7 +62,7 @@ plots_b = map(unique(results.year)) do year
         xticks=1:10)#xlabel="Age class", ylabel="Million tons")
     # scatter!(p, [1], [0], alpha=0, label=year)
 end
-plot(plots_b..., layout=(5, 2), size=(600, 600), left_margin=20px)
+plot(plots_b..., layout=(10, 1), size=(600, 900), left_margin=20px)
 savefig(joinpath(@__DIR__, "biomass.png"))
 
 
@@ -80,23 +80,55 @@ errors = map(ebs_error_files) do f
 end
 errors = vcat(errors...)
 
+
+function resample_df(df, stochastic=true)
+    n = nrow(df)
+    if stochastic
+        ii = sample(1:n, n)
+    else
+        ii = 1:n
+    end
+    return @view df[ii, :]
+end
+
+stds_boot = map(1:1000) do i
+    df = resample_df(errors)
+    @by(df, [:year, :error_label, :variable],
+        :cv = std(:value) / mean(:value)
+    )
+end 
+stds_boot = vcat(stds_boot...)
+summary_boot = @chain stds_boot begin
+    @by([:year, :error_label, :variable],
+        :cv = mean(:cv),
+        :cv_se = std(:cv)
+    )
+    @transform(:error_label = CategoricalArray(:error_label, 
+        levels=["Calibration", "Spatial sampling", "Trawl jackknife", "Trawl assignment", 
+        "Selectivity", "Resample catches", "Length-weight", "TS models", "Age-length", "All"])
+    )
+    @orderby(:year)
+end
+
 error_summary = @chain errors begin
     @by([:year, :added_error, :error_label, :variable],
         :std = std(:value),
         :mean = mean(:value))
     DataFramesMeta.@transform(:cv = :std ./ :mean)
+    @subset(:error_label .!= "All")
 end
+
 error_summary = DataFramesMeta.@transform(error_summary, 
     :error_label = CategoricalArray(:error_label, 
         levels=["Calibration", "Spatial sampling", "Trawl jackknife", "Trawl assignment", 
-        "Resample catches", "Length-weight", "TS models", "Age-length", "All"])
+        "Selectivity", "Resample catches", "Length-weight", "TS models", "Age-length", "All"])
 )
 
 pe1 = @df @subset(error_summary, :variable.=="n") plot(:year, :cv, group=:error_label,
-    ylabel="C.V. (Numbers)", palette=:Set1_9)
+    ylabel="C.V. (Numbers)", palette=:tableau_10)
 pe2 = @df @subset(error_summary, :variable.=="biomass") plot(:year, :cv, group=:error_label,
-    ylabel="C.V. (Biomass)", palette=:Set1_9)
-plot(pe1, pe2, marker=:o, layout=(2,1), legend=:outerright, ylims=(-0.01, 0.13),
-    linewidth=3, xticks=2012:2022, size=(1000, 600), margin=20px)
+    ylabel="C.V. (Biomass)", palette=:tableau_10)
+plot(pe1, pe2, marker=:o, layout=(2,1), legend=:outerright, ylims=(-0.01, 0.26),
+    linewidth=3, xticks=2007:2022, size=(1000, 600), margin=20px)
 savefig(joinpath(@__DIR__, "error_timeseries.png"))
 
