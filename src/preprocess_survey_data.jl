3b4faef281f9bab10afdafe84bb55b93104435cc
Sam Urmy <oceanographerschoice@gmail.com>
2023-11-29 13:16:33 -0800

    preprocess_survey_data docstring

--- src/preprocess_survey_data.jl
+++ src/preprocess_survey_data.jl
@@ -1,10 +1,29 @@
-# using StatsPlots
-# using CSV, DataFrames, DataFramesMeta
-# using Geodesy
-# using ConcaveHull
-# using Statistics
-# using LinearAlgebra
 
+"""
+    preprocess_survey_data(surveydir[, dx=0.0, dy=dx])
+
+Preprocess the survey data files in directory `surveydir` and save the outputs in 
+the same directory as .csv files in standard format. The optional arguments `dx` and `dy`
+set the resolution of the sampling grid; they default to 10.0 (km).
+
+This function expects to find the following files, all of which come from running
+"download_survey.R":
+
+- scaling.csv : Specimen data from scaling_key_source_data
+- acoustics.csv : Acoustic NASC in 0.5 nmi intervals
+- trawl_locations.csv : Lat/Lon locations of all trawl events
+- measurements.csv : Length-weight measurements
+
+The preprocessing includes the following tasks:
+
+- Excluding NASC from non-scaling classes
+- Geographic projection of spatial data
+- Downscaling acoustic resolution (specified by `dx` and `dy` arguments)
+- Calculating survey domain as concave hull of transect ends
+- Setting up simulation grid 
+- General tidying.
+
+"""
 function preprocess_survey_data(surveydir, dx=10.0, dy=dx)
     scaling = CSV.read(joinpath(surveydir, "scaling.csv"), DataFrame)
     scaling_classes = unique(scaling.class)
@@ -38,11 +57,6 @@ function preprocess_survey_data(surveydir, dx=10.0, dy=dx)
         DataFramesMeta.@transform(:lla = LLA.(:eqlatitude, :eqlongitude, 0.0))
         DataFramesMeta.@transform(:utm = [UTM(x, utmzone, true, wgs84) for x in :lla])
         DataFramesMeta.@transform(:x = [u.x / 1e3 for u in :utm], :y = [u.y / 1e3 for u in :utm])
-    end
-    trawl_locations[!, :edsu_idx] .= 0
-    for i in 1:nrow(trawl_locations)
-        y = trawl_locations.y[i]
-        trawl_locations[i, :edsu_idx] = argmin(norm.(y - s for s in acoustics.y))
     end
 
     length_weight = CSV.read(joinpath(surveydir, "measurements.csv"), DataFrame)
