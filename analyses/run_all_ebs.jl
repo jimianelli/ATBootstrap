d0165aa3c5ba110c269ae183cdd49f76bc019d50
Sam Urmy <oceanographerschoice@gmail.com>
2024-03-19 22:06:35 +1100

    script to run all ebs analyses at once

---
+++ analyses/run_all_ebs.jl
@@ -0,0 +1,20 @@
+using Random
+
+Random.seed!(200707)
+
+ebs_surveys = [
+    "200707",
+    "200809",
+    "200909",
+    "201006",
+    "201207",
+    "201407",
+    "201608",
+    "201807",
+    "202207"
+]
+
+for survey in ebs_surveys
+    println("Running analysis for survey $(survey)")
+    include(joinpath(@__DIR__, "dy$(survey).jl"))
+end
