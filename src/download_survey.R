ba2dacfde6ca76798ca2b79af5b1397eb22d57b6
Sam Urmy <oceanographerschoice@gmail.com>
2024-09-27 16:15:16 +1000

    Queries and downloaded data for age-length specimens

--- src/download_survey.R
+++ src/download_survey.R
@@ -150,26 +150,45 @@ download_length_weight_measurements <- function(connection, survey) {
 
 download_age_length_key <- function(connection, survey, data_set_id, analysis_id) {
   print("Downloading age-length keys...")
-  age_length = dbGetQuery(connection,
+  age_length_key = dbGetQuery(connection,
                                glue("SELECT *
           FROM macebase2.age_length_key_data
           WHERE
           macebase2.age_length_key_data.survey = {survey}
           AND macebase2.age_length_key_data.data_set_id = {data_set_id}
           AND macebase2.age_length_key_data.analysis_id = {analysis_id};"))
-  age_length <- cleanup(age_length)
-  totals = age_length %>%
+  age_length_key <- cleanup(age_length_key)
+  totals = age_length_key %>%
     group_by(survey, length) %>%
     summarize(ptotal = sum(proportion_in_key), .groups="drop")
 
-  age_length = age_length %>%
+  age_length_key = age_length_key %>%
     group_by(survey, age, length) %>%
     summarize(proportion = sum(proportion_in_key), .groups="drop") %>%
     left_join(totals, by=c("survey", "length")) %>%
     mutate(proportion = proportion / ptotal) %>%
     select(survey, length, age, proportion)
 
-  return(age_length)
+  return(age_length_key)
+}
+
+download_age_length_measurements <- function(connection, survey) {
+  query = glue("select a.ship, a.survey, a.event_id, a.species_code, a.specimen_id,
+      a.fork_length,a.organism_weight,a.age, a.sex, b.eq_longitude 
+    FROM
+    clamsbase2.v_age_length a 
+    JOIN clamsbase2.v_event_data b ON a.ship=b.ship
+    AND
+    a.survey=b.survey
+    AND
+    a.event_id=b.event_id
+    WHERE
+    a.survey = {survey} 
+    AND
+    a.species_code=21740;")
+  age_length_measurements = dbGetQuery(connection, query)
+  age_length_measurements <- cleanup(age_length_measurements)
+  return(age_length_measurements)
 }
 
 fixlongitude <- function(lon) ifelse(lon > 0, -(360-lon), lon)
@@ -221,10 +240,11 @@ download_survey <- function(connection, survey, data_set_id, analysis_id, ebs=TR
   if (survey == 201608) {
     # hard-coded dataset 1 here because there is no dataset 2 for age-length data in 2016.
     # This is an ugly hack (obviously...)
-    age_length <- download_age_length_key(afsc, survey, 1, analysis_id)
+    age_length_key <- download_age_length_key(afsc, survey, 1, analysis_id)
   } else {
-    age_length <- download_age_length_key(afsc, survey, data_set_id, analysis_id)
+    age_length_key <- download_age_length_key(afsc, survey, data_set_id, analysis_id)
   }
+  age_length <- download_age_length_measurements(afsc, survey)
   acoustics <- download_acoustics(afsc, survey)
 
   surveydir = paste0("surveydata/", survey)
@@ -232,6 +252,7 @@ download_survey <- function(connection, survey, data_set_id, analysis_id, ebs=TR
   write.csv(trawl_locations, paste0(surveydir, "/trawl_locations_mace.csv"))
   write.csv(scaling, paste0(surveydir, "/scaling_mace.csv"))
   write.csv(length_weight, paste0(surveydir, "/measurements.csv"))
+  write.csv(age_length_key, paste0(surveydir, "/age_length_key.csv"))
   write.csv(age_length, paste0(surveydir, "/age_length.csv"))
   write.csv(acoustics, paste0(surveydir, "/acoustics.csv"))
   if (ebs) {
